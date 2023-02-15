e7d664f95cf9a51f2595dedc2ef994b0bd658bf2
FabioLddo <loddo9915@gmail.com>
2023-02-15 23:16:34 +0100

    The plotting function works

--- src/visualizations/visualizations.py
+++ src/visualizations/visualizations.py
@@ -16,48 +16,55 @@ plot_path: Path = Path(Path(__file__).parent, "plots")
 
 
 # Functions:
-# def plot_count_linechart(df: pd.DataFrame, news_type: Literal["positive", "negative", "neutral"]) -> None:
-def plot_count_linechart(df: pd.DataFrame, file_name) -> None:
-    """
-    Plot the correlation matrix.
-    :param df: pd.DataFrame: the dataframe to compute the correlation matrix from.
-    :return: None
-    """
-
+def plot_linechart(df: pd.DataFrame, file_name) -> None:
     # df["month"] = df["month"].dt.strftime('%Y-%m')
     df["year"] = df["month"].dt.year
 
     # Mean line plot
     plt.figure(figsize=(12, 10))
-    sns.lineplot(x='year', y='sentiment_score', data=df)
+    sns.lineplot(x='year', y='sentiment_score', data=df, errorbar=None)
     # Setting Ticks
-    plt.tick_params(axis='x', labelsize=5, rotation=90)
+    plt.tick_params(axis='x', labelsize=15, rotation=90)
     plt.tight_layout()
 
     plt.savefig(Path(plot_path, f'{file_name}_lineplot.png'))
     plt.close()
 
-    # Create a figure with multiple subplots
-    fig, ax = plt.subplots()
 
-    # Plot first sns.linplot on the first subplot
-    sns.lineplot(x="year", y="Negative", data=df, ax=ax)
+def plot_count_linechart(df: pd.DataFrame, file_name) -> None:
+    df["year"] = df["month"].dt.year
 
-    # Plot second sns.linplot on the second subplot
-    sns.lineplot(x="year", y="Positive", data=df, ax=ax)
+    sns.lineplot(x="year", y="sentiment_score", hue='bins', data=df, estimator="count", errorbar=None)
 
-    # Show the plot
-    plt.show()
+    # Set axis labels
+    plt.xlabel("Month")
+    plt.ylabel("Count")
 
-    # fig.savefig(Path(plot_path, f'{file_name}_countplot.png'))
-    # plt.close()
+    plt.savefig(Path(plot_path, f'{file_name}_countplot.png'))
+    plt.close()
 
 
-def plot_frequency_linechart(df: pd.DataFrame, news_type: Literal["positive", "negative", "neutral"]) -> None:
-    # create area chart
-    plt.stackplot(df.period, df.team_B, df.team_C,
-                  labels=['Team B', 'Team C'],
-                  colors=color_map)
+def plot_frequency_linechart(df: pd.DataFrame, file_name) -> None:
+    # Calculate the total number of tips for each month
+    total_tips = df.groupby("month")["sentiment_score"].count()
+
+    # Calculate the frequency of each tip category for each month
+    category_freq = df.groupby(["month", "bins"])["sentiment_score"].count() / total_tips
+
+    # Reset the index of the category_freq DataFrame
+    category_freq = category_freq.reset_index()
+
+    category_freq["year"] = category_freq["month"].dt.year
+
+    # Create a lineplot with the frequency of the categories over time
+    sns.lineplot(data=category_freq, x="year", y="sentiment_score", hue="bins", errorbar=None)
+
+    # Set axis labels
+    plt.xlabel("Month")
+    plt.ylabel("Frequency")
+
+    plt.savefig(Path(plot_path, f'{file_name}_freqplot.png'))
+    plt.close()
 
 
 def plot_word_cloud(df: pd.DataFrame, file_name) -> None:
@@ -78,27 +85,6 @@ def plot_word_cloud(df: pd.DataFrame, file_name) -> None:
 
 
 def main() -> None:
-    '''
-    # Only egypt for the moment
-    data_path: Path = Path(Path(__file__).parent.parent, "clean_data", "C_Egyptnyt.csv")
-    df = pd.read_csv(data_path)
-    print(df.pub_date)
-
-    # assuming your dataframe is called df
-
-    
-    df['pub_date'] = pd.to_datetime(df['pub_date'])
-    df['month'] = df['pub_date'].dt.to_period('M')
-
-    # group by month and aggregate the data as needed
-    df_grouped = df.groupby('month').agg({'CleanedText': 'sum'}).reset_index()
-
-    print(df_grouped.info())
-
-    # Plot the word cloud:
-    # plot_word_cloud(df_grouped)
-    '''
-
     # get a list of files in the folder
     file_list = os.listdir("../../data/nyt_data")
 
@@ -106,11 +92,9 @@ def main() -> None:
     csv_files = [f for f in file_list if f.endswith('.csv')]
 
     # loop through the list and read the CSV files, storing their names and data
-    data = []
     for csv_file in csv_files:
         file_path = os.path.join("../../data/nyt_data", csv_file)
         df = pd.read_csv(file_path)
-        # print(df.info())
 
         # assuming your dataframe is called df
         df['pub_date'] = pd.to_datetime(df['pub_date'])
@@ -118,24 +102,18 @@ def main() -> None:
 
         # assuming your DataFrame is called df and the numerical feature is called 'num_feature'
         bin_labels = ['Negative', 'Neutral', 'Positive']
-        num_quartiles = 3
-        # df['bins'] = pd.qcut(df['sentiment_score'], q=num_quartiles, labels=bin_labels)
         df['bins'] = pd.cut(df['sentiment_score'], bins=[-1, -0.5, 0.5, 1], labels=bin_labels)
 
-        dummy = pd.get_dummies(df['bins'])
+        plot_linechart(df, csv_file)
 
-        # concatenate the original dataframe with the dummy variables
-        df = pd.concat([df, dummy], axis=1)
+        plot_count_linechart(df, csv_file)
 
-        print(df.info())
+        plot_frequency_linechart(df, csv_file)
 
         # group by month and aggregate the data as needed
-        df_grouped = df.groupby('month').agg({'CleanedText': 'sum', 'sentiment_score': 'mean', 'Negative': 'count',
-                                              'Neutral': 'count', 'Positive': 'count'}).reset_index()
+        df_grouped = df.groupby('month').agg({'CleanedText': 'sum', 'sentiment_score': 'mean'}).reset_index()
 
-        # plot_word_cloud(df_grouped, csv_file)
-
-        plot_count_linechart(df_grouped, csv_file)
+        plot_word_cloud(df_grouped, csv_file)
 
     # df['Publish_Date'] = pd.to_datetime(df['Publish_Date'], unit='s')
 
