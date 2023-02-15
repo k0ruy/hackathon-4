9626303b1a47539a3cee212c31681663af8fa69d
C. Berchtold <christian.berchtold@student.supsi.ch>
2023-02-15 08:42:57 +0100

    var name changes

--- scraperv2.py
+++ scraperv2.py
@@ -3,6 +3,8 @@ import pandas as pd
 import json
 import os
 
+# source: https://blog.devgenius.io/scraping-reddit-with-praw-python-reddit-api-wrapper-eaa7d788d7b9
+
 
 if __name__ == '__main__':
 
@@ -18,30 +20,31 @@ if __name__ == '__main__':
                     user_agent="Maleficent-Cow-1318",
                     )
 
-    topics = ["Bahrain", "Cyprus", "Egypt", "Iran", "Iraq", "Israel", "Jordan", "Kuwait", "Lebanon", "Oman", "Palestine", "Qatar", "Saudi Arabia", "Syria", "Turkey", "United Arab Emirates", "Yemen"]
+    topics = ["Bahrain", "Cyprus", "Egypt", "Iran", "Iraq", "Israel", "Jordan", "Kuwait", "Lebanon", "Oman",
+              "Palestine", "Qatar", "Saudi Arabia", "Syria", "Turkey", "United Arab Emirates", "Yemen"]
     sub = 'worldnews'  # subreddit
     sort = "new"
     limit = 50
     for q in topics:
-        top_posts = r.subreddit(sub).search(q, sort=sort, limit=limit)
-        total_posts = list()
-        for post in top_posts:
+        new_posts = r.subreddit(sub).search(q, sort=sort, limit=limit)
+        all_posts = list()
+        for post in new_posts:
             # print(vars(post)) # print all properties
-            Title = post.title,
-            Score = post.score,
-            Number_Of_Comments = post.num_comments,
-            Publish_Date = post.created,
-            Link = post.permalink,
-            data_set = {"Title": Title[0], "Score": Score[0], "Number_Of_Comments": Number_Of_Comments[0],
-                        "Publish_Date": Publish_Date[0], "Link": 'https://www.reddit.com' + Link[0]}
-            total_posts.append(data_set)
+            title = post.title,
+            score = post.score,
+            n_comments = post.num_comments,
+            pub_date = post.created,
+            link = post.permalink,
+            res = {"Title": title[0], "Score": score[0], "Number_Of_Comments": n_comments[0],
+                   "Publish_Date": pub_date[0], "Link": 'https://www.reddit.com' + link[0]}
+            all_posts.append(res)
 
             # create csv file
-            df = pd.DataFrame(total_posts)
+            df = pd.DataFrame(all_posts)
             df.to_csv(f'data/data_key_{q}.csv', sep=',', index=False)
 
             # create json file
-            json_string = json.dumps(total_posts)
+            json_string = json.dumps(all_posts)
             jsonFile = open(f'data/data_key_{q}.json', "w")
             jsonFile.write(json_string)
             jsonFile.close()
