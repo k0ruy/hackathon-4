a4dba7f5978a6851fd8cdb21c08c71eb43a9beb7
FabioLddo <loddo9915@gmail.com>
2023-02-16 08:22:56 +0100

    modified file names

--- src/visualizations/visualizations.py
+++ src/visualizations/visualizations.py
@@ -1,50 +1,60 @@
 # Data manipulation:
 from pathlib import Path
 import os
-from datetime import datetime
 # Plotting:
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-# Type hinting:
-from typing import Literal
+
 
 plot_path: Path = Path(Path(__file__).parent, "plots")
+if not plot_path:
+    plot_path.mkdir(parents=True, exist_ok=True)
 
 
 # Functions:
-def plot_linechart(df: pd.DataFrame, file_name) -> None:
+def plot_linechart(df: pd.DataFrame, file_name: str) -> None:
     # df["month"] = df["month"].dt.strftime('%Y-%m')
     df["year"] = df["month"].dt.year
 
     # Mean line plot
     plt.figure(figsize=(12, 10))
     sns.lineplot(x='year', y='sentiment_score', data=df, errorbar=None)
+
+    plt.title(file_name)
+
+    # Set axis labels
+    plt.xlabel("Year")
+    plt.ylabel("Mean score")
+
     # Setting Ticks
-    plt.tick_params(axis='x', labelsize=15, rotation=90)
+    # plt.tick_params(axis='x', labelsize=15, rotation=90)
     plt.tight_layout()
 
     plt.savefig(Path(plot_path, f'{file_name}_lineplot.png'))
     plt.close()
 
 
-def plot_count_linechart(df: pd.DataFrame, file_name) -> None:
+def plot_count_linechart(df: pd.DataFrame, file_name: str) -> None:
     df["year"] = df["month"].dt.year
 
     sns.lineplot(x="year", y="sentiment_score", hue='bins', data=df, estimator="count", errorbar=None)
 
+    plt.title(file_name)
+
     # Set axis labels
-    plt.xlabel("Month")
+    plt.xlabel("Year")
     plt.ylabel("Count")
+
+    plt.tight_layout()
 
     plt.savefig(Path(plot_path, f'{file_name}_countplot.png'))
     plt.close()
 
 
-def plot_frequency_linechart(df: pd.DataFrame, file_name) -> None:
+def plot_frequency_linechart(df: pd.DataFrame, file_name: str) -> None:
     # Calculate the total number of tips for each month
     total_tips = df.groupby("month")["sentiment_score"].count()
 
@@ -59,9 +69,13 @@ def plot_frequency_linechart(df: pd.DataFrame, file_name) -> None:
     # Create a lineplot with the frequency of the categories over time
     sns.lineplot(data=category_freq, x="year", y="sentiment_score", hue="bins", errorbar=None)
 
+    plt.title(file_name)
+
     # Set axis labels
-    plt.xlabel("Month")
+    plt.xlabel("Year")
     plt.ylabel("Frequency")
+
+    plt.tight_layout()
 
     plt.savefig(Path(plot_path, f'{file_name}_freqplot.png'))
     plt.close()
@@ -94,6 +108,7 @@ def main() -> None:
     # loop through the list and read the CSV files, storing their names and data
     for csv_file in csv_files:
         file_path = os.path.join("../../data/nyt_data", csv_file)
+        file_name = csv_file.split('.')[0]
         df = pd.read_csv(file_path)
 
         # assuming your dataframe is called df
@@ -104,16 +119,16 @@ def main() -> None:
         bin_labels = ['Negative', 'Neutral', 'Positive']
         df['bins'] = pd.cut(df['sentiment_score'], bins=[-1, -0.5, 0.5, 1], labels=bin_labels)
 
-        plot_linechart(df, csv_file)
+        plot_linechart(df, file_name)
 
-        plot_count_linechart(df, csv_file)
+        plot_count_linechart(df, file_name)
 
-        plot_frequency_linechart(df, csv_file)
+        plot_frequency_linechart(df, file_name)
 
         # group by month and aggregate the data as needed
         df_grouped = df.groupby('month').agg({'CleanedText': 'sum', 'sentiment_score': 'mean'}).reset_index()
 
-        plot_word_cloud(df_grouped, csv_file)
+        plot_word_cloud(df_grouped, file_name)
 
     # df['Publish_Date'] = pd.to_datetime(df['Publish_Date'], unit='s')
 
